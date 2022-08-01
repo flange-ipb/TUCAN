6c255b0dbd98daab6fcd729b930323c073140e47
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2022-08-01 14:35:49 +0200

    poor man's debugging

--- tests/test_canonicalization.py
+++ tests/test_canonicalization.py
@@ -17,15 +17,35 @@ def test_permutation(m):
     assert m.edges != m_permu.edges
 
 
-def test_invariance(m, n_runs=10, random_seed=random.random()):
+def _print_graph(m: nx.Graph):
+    [print(n) for n in list(m.nodes(data=True))]
+    [print(n) for n in list(m.edges(data=True))]
+
+
+def test_invariance(m: nx.Graph, n_runs=10, random_seed=random.random()):
     """Eindeutigkeit."""
-    m_canon = canonicalize_molecule(m)
+    print("\nOriginal Graph:")
+    _print_graph(m)
+
+    m_canon = canonicalize_molecule(m.copy())
     m_serialized = serialize_molecule(m_canon)
     random.seed(random_seed)
+
+    print("\nCanonicalized Graph:")
+    _print_graph(m_canon)
+
     for _ in range(n_runs):
         permutation_seed = random.random()
-        m_permu = permute_molecule(m, random_seed=permutation_seed)
-        m_permu_canon = canonicalize_molecule(m_permu)
+        m_permu = permute_molecule(m.copy(), random_seed=permutation_seed)
+
+        print("\nPermuted Graph:")
+        _print_graph(m_permu)
+
+        m_permu_canon = canonicalize_molecule(m_permu.copy())
+
+        print("\nPermuted-and-Canonicalized Graph:")
+        _print_graph(m_permu_canon)
+
         m_permu_serialized = serialize_molecule(m_permu_canon)
         assert m_serialized == m_permu_serialized
 
